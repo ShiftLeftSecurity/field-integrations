a451a534cbbf17a8e51591eb515c9e087b066ad6
Prabhu Subramanian <prabhu@shiftleft.io>
2022-03-23 16:44:36 +0000

    Added gitlab template

---
+++ ci-templates/sl-analysis.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Invoke maven and specify the target director here
+# For javascript, we look for package.json
+TARGET_DIR=target
+if [ -d "$TARGET_DIR" ]; then
+    jar cvf app.jar -C $TARGET_DIR .
+    sl analyze --wait --app "$CI_PROJECT_NAME" --version-id "$CI_COMMIT_SHA" --tag branch="$CI_COMMIT_REF_NAME" --java app.jar
+elif [ -e "package.json" ]; then
+    sl analyze --wait --app "$CI_PROJECT_NAME" --version-id "$CI_COMMIT_SHA" --tag branch="$CI_COMMIT_REF_NAME" --js .
+fi
+# Check if this is running in a merge request
+if [ -n "$CI_MERGE_REQUEST_IID" ] && [ -n "$MR_TOKEN" ]; then
+  echo "Got merge request $CI_MERGE_REQUEST_IID for branch $CI_COMMIT_REF_NAME"
+
+  # Run check-analysis and save report to /tmp/check-analysis.md
+  sl check-analysis \
+    --app "$CI_PROJECT_NAME" \
+    --report \
+    --report-file /tmp/check-analysis.md \
+    --source "tag.branch=master" \
+    --target "tag.branch=$CI_COMMIT_REF_NAME"
+
+  CHECK_ANALYSIS_OUTPUT=$(cat /tmp/check-analysis.md)
+  COMMENT_BODY=$(jq -n --arg body "$CHECK_ANALYSIS_OUTPUT" '{body: $body}')
+
+  # Post report as merge request comment
+  curl -i -XPOST "https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/merge_requests/$CI_MERGE_REQUEST_IID/notes" \
+    -H "PRIVATE-TOKEN: $MR_TOKEN" \
+    -H "Content-Type: application/json" \
+    -d "$COMMENT_BODY"
+fi
+
