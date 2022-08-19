0e29e8a3742bd656404876336ea244e41f44811c
Yusef Saraby <ysaraby@shiftleft.io>
2022-08-20 00:05:40 +0300

    Apply pr comments

--- cli-examples/cli-analyze-demo-applicaitons.sh
+++ cli-examples/cli-analyze-demo-applicaitons.sh
@@ -4,8 +4,19 @@
 # Set absolute path to working directory.
 # "/home/user/projects/projectfolder" or "/home/user/projects/projectfolder/project.jar"
 DOCKER_WORK_DIR="/Users/yusefsaraby/projects/testing" # <-- Update per comment above
-DOCKER_IMAGE_NAME=shiftleft/scan:latest
+DOCKER_IMAGE_NAME=shiftleft/core:latest
+
+# User defined repositories
+GO_REPO=https://github.com/ShiftLeftSecurity/shiftleft-go-demo.git
+JAVA_REPO=https://github.com/ShiftLeftSecurity/shiftleft-java-demo.git
+PYTHON_REPO=https://github.com/ShiftLeftSecurity/shiftleft-python-demo.git
+JS_REPO=https://github.com/ShiftLeftSecurity/shiftleft-js-demo.git
+
 ERR_NOT_FOUND=126
+# SUMARY_GO=0
+# SUMMARY_JAVA=0
+# SUMMARY_PYTHON=0
+# SUMMARY_JS=0
 
 # Variable Cleanup
 ## Remove trailing slash(es) if present
@@ -24,45 +35,59 @@ docker pull $DOCKER_IMAGE_NAME
 # $2: absolute path to code or binaries
 shiftleft_analyze_code() {
     echo "Will analyze $1 code at location $2"
-    docker run --rm -e SHIFTLEFT_ACCESS_TOKEN -v "$2":/myvol -it $DOCKER_IMAGE_NAME sl analyze --wait --app shiftleft-js-demo --"$1" /myvol
+    docker run --rm -e SHIFTLEFT_ACCESS_TOKEN -v "$2":/myvol -it $DOCKER_IMAGE_NAME sl analyze --wait --container $DOCKER_IMAGE_NAME --app shiftleft-js-demo "--$1" /myvol
 }
 
 # $1: absolute path to code or binaries
 run_demo_go() {
     echo "Analyzing Go project: code at location $1"
     shiftleft_analyze_code "go" "$1"
-    docker run --rm -e SHIFTLEFT_ACCESS_TOKEN -v "$1":/myvol -it $DOCKER_IMAGE_NAME /bin/bash -c "cd /myvol; go build; sl analyze --wait --app shiftleft-go-demo --go /myvol"
+    docker run --rm -e SHIFTLEFT_ACCESS_TOKEN -v "$1":/myvol -it $DOCKER_IMAGE_NAME /bin/bash -c "cd /myvol; go build; sl analyze --wait --container $DOCKER_IMAGE_NAME --app shiftleft-go-demo --go /myvol"
 }
 
 # $1: absolute path to code or binaries
 run_demo_java() {
     echo "Analyzing JAVA project: code at location $1"
     shiftleft_analyze_code "java" "$1"
-    docker run --rm -e SHIFTLEFT_ACCESS_TOKEN -v "$1":/myvol -it $DOCKER_IMAGE_NAME /bin/bash -c "cd /myvol; mvn clean package; sl analyze --wait --app shiftleft-java-demo --java /myvol/target/hello-shiftleft-0.0.1.jar"
+    docker run --rm -e SHIFTLEFT_ACCESS_TOKEN -v "$1":/myvol -it $DOCKER_IMAGE_NAME /bin/bash -c "cd /myvol; mvn clean package; sl analyze --wait --container $DOCKER_IMAGE_NAME --app shiftleft-java-demo --java /myvol/target/hello-shiftleft-0.0.1.jar"
 }
 
 # $1: absolute path to code or binaries
 run_demo_python() {
     echo "Analyzing PYTHON project: code at location $1"
     shiftleft_analyze_code "python" "$1"
-    docker run --rm -e SHIFTLEFT_ACCESS_TOKEN -v "$1":/myvol -it $DOCKER_IMAGE_NAME /bin/bash -c "cd /myvol; pip install -r requirements.txt; sl analyze --wait --app shiftleft-python-demo --python /myvol"
+    docker run --rm -e SHIFTLEFT_ACCESS_TOKEN -v "$1":/myvol -it $DOCKER_IMAGE_NAME /bin/bash -c "cd /myvol; pip install -r requirements.txt; sl analyze --wait --container $DOCKER_IMAGE_NAME --app shiftleft-python-demo --python /myvol"
+    # docker run --rm -e SHIFTLEFT_ACCESS_TOKEN -v "$1":/myvol -it $DOCKER_IMAGE_NAME /bin/bash -c "cd /myvol; pip install -r requirements.txt; sl analyze --wait --container $DOCKER_IMAGE_NAME --app shiftleft-python-demo --python /myvol; sl check-analysis --v2 --source scan.1  --target scan.2 --app $(basename $GO_REPO) --config /myvol/$REPO_NAME_PYTHON/shiftleft.yml"
+}
+
+# $1: Clone Command exit code
+# $2: Repository language
+# $3: Repository Clone URL
+failed_to_clone() {
+   # We exit in failed to clone, this usually means network or write perm issues, if one fails others are likely to
+    echo "Failed to clone $2 repository from $3"
+    echo "Stopping now"
+    exit "$1"
 }
 
 # SL to analyze demo applications
 echo "************** GO *************"
-git clone https://github.com/ShiftLeftSecurity/shiftleft-go-demo.git || true
-run_demo_go "$DOCKER_WORK_DIR/shiftleft-go-demo" || true
+DOCKER_REPO_GO="$DOCKER_WORK_DIR/$(basename $GO_REPO)"
+git clone $GO_REPO "$DOCKER_REPO_GO" || true
+run_demo_go "$DOCKER_REPO_GO" || true
 
 echo "************** JAVA *************"
-git clone https://github.com/ShiftLeftSecurity/shiftleft-java-demo.git || true
-run_demo_java "$DOCKER_WORK_DIR/shiftleft-java-demo" || true
+DOCKER_REPO_JAVA="$DOCKER_WORK_DIR/$(basename $JAVA_REPO)"
+git clone $JAVA_REPO "$DOCKER_REPO_JAVA" || true
+run_demo_java "$DOCKER_REPO_JAVA" || true
 
 echo "************** PYTHON *************"
-git clone https://github.com/ShiftLeftSecurity/shiftleft-python-demo.git || true
-run_demo_python "$DOCKER_WORK_DIR/shiftleft-python-demo" || true
-
+REPO_NAME_PYTHON="$(basename $PYTHON_REPO)"
+DOCKER_REPO_PYTHON="$DOCKER_WORK_DIR/$REPO_NAME_PYTHON"
+git clone $PYTHON_REPO "$DOCKER_REPO_PYTHON" || true
+run_demo_python "$DOCKER_REPO_PYTHON" || true
+ 
 echo "************** JS *************"
-git clone https://github.com/ShiftLeftSecurity/shiftleft-js-demo.git || true
-shiftleft_analyze_code "js" "$DOCKER_WORK_DIR/shiftleft-js-demo" || true
-
-echo "DONE"
+DOCKER_REPO_JS="$DOCKER_WORK_DIR/$(basename $JS_REPO)"
+git clone $JS_REPO "$DOCKER_REPO_JS" || true
+shiftleft_analyze_code "js" "$DOCKER_REPO_JS" || true
